989b69335e8429717c890a697fa3c463d6e784b5
Mahdi Chihaoui <almahdichihaoui@gmail.com>
2021-01-27 18:16:49 +0100

    implemented filter function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-const data = require('../data');
 
 const filter = (data, pattern) => {
-    return data;
+    return data.reduce((acc, currCountry) => {
+        const peopleWithGoodAnimals = currCountry
+            .people
+            .reduce((acc, currPerson) => {
+                const goodAnimals = currPerson
+                    .animals
+                    .filter(animal => animal.name.includes(pattern));
+
+                if (goodAnimals.length === 0) {
+                    return acc;
+                }
+
+                return acc.concat([{
+                    name: currPerson.name,
+                    animals: goodAnimals,
+                }])
+            }, [])
+
+        if (peopleWithGoodAnimals.length === 0) {
+            return acc;
+        }
+
+        return acc.concat([{
+            name: currCountry.name,
+            people: peopleWithGoodAnimals,
+        }])
+    }, []);
 }
 
 module.exports = {
-    filterData,
+    filter,
 }
